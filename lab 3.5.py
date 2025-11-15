a3003ba00dda39f59817f28414d45c26565c2356
KupchenkoIrina <irinacrav86@gmail.com>
2025-11-15 19:09:25 +0300

    Add files via upload

---
+++ lab 3.5.py
@@ -0,0 +1,11 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+year = int(input())
+if year % 400 == 0:
+    print("да")
+elif year % 100 == 0:
+    print("нет")
+elif year % 4 == 0:
+    print("да")
+else:
+    print("нет")
